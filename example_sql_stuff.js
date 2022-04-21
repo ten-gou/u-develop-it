71fd4c5412e100643f0f487462c56862b4163d78
Maribel <lwang79@zoho.com>
2022-04-21 14:50:09 -0700

    added the party table, and the party names column to the candidates table

--- example_sql_stuff.js
+++ example_sql_stuff.js
@@ -1,7 +1,19 @@
-// Select a specific id from sql
-// SELECT first_name, last_name, industry_connected
-// FROM candidates
-// WHERE id = 5;
+// views the available database/tables w/in the database
+// SHOW DATABASES/TABLES;
 
 // select a specific database
-// USE <database>
+// USE <database>;
+
+// Select a specific id from sql TABLE
+// SELECT <x, y, z>
+// FROM <table>
+// WHERE <x/y/z> = <value>;
+
+// generates a database/table using sql files
+// SOURCE <path to the sql file>
+
+// alters a preexisting table
+// ALTER TABLE <table> ADD COLUMN <column name> INTEGER;
+
+// deletes a table/database
+// DROP DATABASE/TABLE <name of table/database>;
